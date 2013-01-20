834f50bd85a3b9189cb116a39a255c10b16df3e2
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2013-01-20 14:54:04 +0400

    bytecode tests

--- jss_interp/test/test_bytecode.py
+++ jss_interp/test/test_bytecode.py
@@ -1,12 +1,53 @@
 # -*- encoding: utf-8 -*-
 
-from jss_interp.parser import ConstantNum
+from jss_interp.parser import ConstantNum, Variable, Assignment, Stmt, Block
 from jss_interp.bytecode import compile_ast, \
-        LOAD_CONSTANT, DISCARD_TOP, RETURN
+        LOAD_CONSTANT, RETURN, LOAD_VAR, ASSIGN, DISCARD_TOP
+
+
+def to_code(*bytecode_list):
+    return ''.join(map(chr, bytecode_list))
 
 
 def test_const_num():
     bytecode = compile_ast(ConstantNum(10.0))
-    assert bytecode.code == ''.join(map(chr, [LOAD_CONSTANT, 0, RETURN, 0]))
+    assert bytecode.code == to_code(LOAD_CONSTANT, 0, RETURN, 0)
     assert bytecode.numvars == 0
     assert bytecode.constants == [10.0]
+
+
+def test_variable():
+    bytecode = compile_ast(Variable('x'))
+    assert bytecode.code == to_code(LOAD_VAR, 0, RETURN, 0)
+    assert bytecode.numvars == 1
+    assert bytecode.constants == []
+
+
+def test_assignment():
+    bytecode = compile_ast(Assignment('x', Variable('y')))
+    assert bytecode.code == to_code(LOAD_VAR, 0, ASSIGN, 1, RETURN, 0)
+    assert bytecode.numvars == 2
+    assert bytecode.constants == []
+
+    bytecode = compile_ast(Assignment('x', ConstantNum(13.4)))
+    assert bytecode.code == to_code(LOAD_CONSTANT, 0, ASSIGN, 0, RETURN, 0)
+    assert bytecode.numvars == 1
+    assert bytecode.constants == [13.4]
+
+
+def test_stmt():
+    bytecode = compile_ast(Stmt(Variable('x')))
+    assert bytecode.code == to_code(LOAD_VAR, 0, DISCARD_TOP, 0, RETURN, 0)
+    assert bytecode.numvars == 1
+    assert bytecode.constants == []
+
+
+def test_block():
+    bytecode = compile_ast(Block([
+        Stmt(Variable('x')), Stmt(ConstantNum(12.3))]))
+    assert bytecode.code == to_code(
+            LOAD_VAR, 0, DISCARD_TOP, 0, 
+            LOAD_CONSTANT, 0, DISCARD_TOP, 0,
+            RETURN, 0)
+    assert bytecode.numvars == 1
+    assert bytecode.constants == [12.3]
