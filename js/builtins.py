5a62bc00177250cbea826f2b4dbe5b25344206c3
James Mills <prologic@shortcircuit.net.au>
2015-02-25 23:53:06 +1000

    Added typeof builtin function

--- js/builtins.py
+++ js/builtins.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from js.base_objects import W_BuilinFunction
+from js.base_objects import W_BuilinFunction, W_StringObject
 
 
 class W_PrintFn(W_BuilinFunction):
@@ -9,7 +9,14 @@ class W_PrintFn(W_BuilinFunction):
         print arg_list[0].to_string()
 
 
+class W_TypeOf(W_BuilinFunction):
+
+    def call(self, arg_list):
+        return W_StringObject(type(arg_list[0]).__name__.replace("W_", ""))
+
+
 BUILTINS = {
     'print': W_PrintFn(),
+    'typeof': W_TypeOf(),
     #'assert': W_AssertFn,
 }
